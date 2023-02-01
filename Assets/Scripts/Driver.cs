4e0320461ee74766b5e9b06667f8d6f1587fe617
Cago75 <88317757+Cago75@users.noreply.github.com>
2023-02-01 20:35:02 +0200

    Lesson#17
    Added user controls, rigid bodies and colliders

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 250f;
+    [SerializeField] float driveSpeed = 20f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +17,10 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,0.3f);  
-        transform.Translate(0, 0.01f,0); 
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float DriveAmount = Input.GetAxis("Vertical")   * driveSpeed * Time.deltaTime;
+        transform.Rotate(0,0,-steerAmount);  
+        transform.Translate(0, DriveAmount,0); 
     }
 
 
